ec47673ae251390287d751343f8f74bc4d9fff8b
Administrator <Administrator@7PUBXIFQJ0BCVY4>
2019-08-27 14:45:31 +0800

    config

---
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -0,0 +1,12 @@
+package com.tedu.sp01.pojo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+	private Integer id;
+	private String name;
+	private Integer number;
+}
